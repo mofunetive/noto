559f4792e7096fa1a8fae15152e5759377055e1d
Faelayis <48393914+Faelayis@users.noreply.github.com>
2025-03-30 16:45:05 +0000

    chore: code formatting

--- packages/api/src/services/users/user.ts
+++ packages/api/src/services/users/user.ts
@@ -5,30 +5,28 @@ export interface CreateUserParams {
 }
 
 export async function getOrCreateUser(database: PrismaClient, params: CreateUserParams) {
-   let user = await database.user.findUnique({
-      where: { authId: params.uuid },
-   });
+	let user = await database.user.findUnique({
+		where: { authId: params.uuid },
+	});
 
-   if (!user) {
-      const authUser = await database.users.findUnique({
-         where: { id: params.uuid },
-      });
+	if (!user) {
+		const authUser = await database.users.findUnique({
+			where: { id: params.uuid },
+		});
 
-      if (!authUser) {
-         throw new Error(`Auth user with id ${params.uuid} not found`);
-      }
+		if (!authUser) {
+			throw new Error(`Auth user with id ${params.uuid} not found`);
+		}
 
-      user = await database.user.create({
-         data: {
-         name: typeof authUser.raw_user_meta_data === "string" 
-            ? JSON.parse(authUser.raw_user_meta_data).full_name ?? "Unknown User" 
-            : "Unknown User",
-         email: authUser.email,
-         authId: authUser.id,
-         createdAt: new Date(),
-         },
-      });
-   }
+		user = await database.user.create({
+			data: {
+				name: typeof authUser.raw_user_meta_data === "string" ? (JSON.parse(authUser.raw_user_meta_data).full_name ?? "Unknown User") : "Unknown User",
+				email: authUser.email,
+				authId: authUser.id,
+				createdAt: new Date(),
+			},
+		});
+	}
 
-   return user;
+	return user;
 }
