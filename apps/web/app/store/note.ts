76815bb963e25fb5244aade8e76e9a85b01f9f4d
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-04-04 18:37:25 +0700

    fix: update package versions to remove caret (^) for consistency

--- apps/web/app/store/note.ts
+++ apps/web/app/store/note.ts
@@ -10,28 +10,28 @@ interface State {
 	removeNote: (noteId: number) => void;
 }
 
-// const openIndexDB = async () => {
-// 	await openDB("noto", 3, {
-// 		upgrade(db) {
-// 			if (!db.objectStoreNames.contains("notes")) {
-// 				const objectStore = db.createObjectStore("notes", { keyPath: "id", autoIncrement: true });
-// 				objectStore.createIndex("id", "id");
-// 				objectStore.createIndex("title", "title");
-// 				objectStore.createIndex("content", "content");
-// 				objectStore.createIndex("createdAt", "createdAt");
-// 				objectStore.createIndex("updatedAt", "updatedAt");
-// 				objectStore.createIndex("userId", "userId");
+const openIndexDB = async () => {
+	return await openDB("noto", 3, {
+		upgrade(db) {
+			if (!db.objectStoreNames.contains("notes")) {
+				const objectStore = db.createObjectStore("notes", { keyPath: "id", autoIncrement: true });
+				objectStore.createIndex("id", "id");
+				objectStore.createIndex("title", "title");
+				objectStore.createIndex("content", "content");
+				objectStore.createIndex("createdAt", "createdAt");
+				objectStore.createIndex("updatedAt", "updatedAt");
+				objectStore.createIndex("userId", "userId");
 
-// 				console.log("Store Created");
-// 			}
-// 		},
-// 	});
-// };
+				console.log("Store Created");
+			}
+		},
+	});
+};
 
 export const useNoteStore = create<State>((set) => ({
 	allNotes: [],
 	getNotes: async () => {
-		const db = await openDB("noto", 3);
+		const db = await openIndexDB();
 		const tx = db.transaction("notes", "readonly");
 		const store = tx.objectStore("notes");
 
@@ -57,6 +57,7 @@ export const useNoteStore = create<State>((set) => ({
 		if (note.length > 0) {
 			note[0].title = title;
 			note[0].content = content;
+			note[0].updatedAt = new Date();
 
 			const db = await openDB("noto", 3);
 			const tx = db.transaction("notes", "readwrite");
