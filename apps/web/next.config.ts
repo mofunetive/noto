dc85f2bea545e93bf379620cc8537fb6c4216814
Faelayis <48393914+Faelayis@users.noreply.github.com>
2025-04-05 19:56:06 +0700

    fix: streamline dotenv configuration in next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,18 +1,14 @@
 import type { NextConfig } from "next";
 import dotenv from "dotenv";
 
+dotenv.config({ path: "../../.env" });
+
 const nextConfig: NextConfig = {
-	/* config options here */
-};
-
-if (process.env.npm_lifecycle_event === "dev" || process.env.NODE_ENV === "development") {
-	dotenv.config({ path: "../../.env" });
-
-	nextConfig["env"] = {
+	env: {
 		DATABASE_URL: process.env.DATABASE_URL,
 		SUPABASE_URL: process.env.SUPABASE_URL,
 		SUPABASE_KEY: process.env.SUPABASE_KEY,
-	};
-}
+	},
+};
 
 export default nextConfig;
